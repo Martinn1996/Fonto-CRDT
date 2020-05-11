fc6147089aee4294ad85927848c7043efef275a0
qcllee <33662960+qcllee@users.noreply.github.com>
2020-05-11 15:42:35 +0200

    Add tests for getters and setters of the Logoot Object (#11)
    * Add tests for getters and setters of the Logoot Object
    
    * Change crdt name
    
    * Make test naming consistent

---
+++ test/LogootGetSetTest.js
@@ -0,0 +1,52 @@
+const assert = require('chai').assert;
+const Logoot = require('../src/CRDT/src/index');
+
+describe('Logoot', () => {
+	let crdt;
+
+	beforeEach(() => {
+		crdt = new Logoot('crdt');
+	});
+
+	describe('value() and setValue()', () => {
+		it('should return empty string when nothing inserted', () => {
+			assert.equal(crdt.value(), '');
+		});
+
+		it('should return the new value when setValue() is called', () => {
+			crdt.setValue('test');
+			assert.equal(crdt.value(), 'test');
+		});
+
+		it('should overwrite the current value when called', () => {
+			crdt.setValue('test');
+			crdt.setValue('new');
+			assert.equal(crdt.value(), 'new');
+		});
+
+		it('should throw error when value is of invalid type', () => {
+			const errorFunction = () => crdt.setValue(1);
+			assert.throws(errorFunction, Error);
+		});
+	});
+
+	describe('getState() and setState()', () => {
+		it('should have exactly the state after setting the state', () => {
+			crdt.setValue('test');
+			const expectedState = crdt.getState();
+			crdt = new Logoot('1');
+			crdt.setState(expectedState);
+			assert.equal(crdt.getState(), expectedState);
+		});
+
+		it('should throw error when state is not a valid state', () => {
+			const errorFunction = () => crdt.setState({ test: 2 });
+			assert.throws(errorFunction, Error);
+		});
+
+		it('should throw error when state is not an object', () => {
+			const errorFunction = () => crdt.setState('test');
+			assert.throws(errorFunction, Error);
+		});
+	});
+});
