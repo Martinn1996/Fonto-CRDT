dbd2ac3cc7d9107b8bfb6d6211ef4fab0a6d1b32
Wang Hao Wang <W.H.Wang@student.tudelft.nl>
2020-05-11 15:31:41 +0200

    Swap actual and expectations in assertions

--- test/deleteTest.js
+++ test/deleteTest.js
@@ -29,56 +29,56 @@ describe('Delete suite', () => {
 	it('(One editor) Deletes nothing', () => {
 		crdt1.delete(0, 0);
 
-		assert.equal(testString, crdt1.value());
-		assert.equal(testString, crdt2.value());
+		assert.equal(crdt1.value(), testString);
+		assert.equal(crdt2.value(), testString);
 		assert.deepEqual(crdt1.getState(), crdt2.getState());
 	});
 
 	it('(One editor) Deletes one character', () => {
 		crdt1.delete(0);
 
-		assert.equal(testString.substring(1, testString.length), crdt1.value());
-		assert.equal(testString.substring(1, testString.length), crdt2.value());
+		assert.equal(crdt1.value(), testString.substring(1, testString.length));
+		assert.equal(crdt2.value(), testString.substring(1, testString.length));
 		assert.deepEqual(crdt1.getState(), crdt2.getState());
 	});
 
 	it('(One editor) Deletes a character with string', () => {
 		crdt1.delete('This');
 
-		assert.equal(testString, crdt1.value());
-		assert.equal(testString, crdt2.value());
+		assert.equal(crdt1.value(), testString);
+		assert.equal(crdt2.value(), testString);
 		assert.deepEqual(crdt1.getState(), crdt2.getState());
 	});
 
 	it('(One editor) Deletes a character with a double', () => {
 		crdt1.delete(2.5);
 
-		assert.equal(testString, crdt1.value());
-		assert.equal(testString, crdt2.value());
+		assert.equal(crdt1.value(), testString);
+		assert.equal(crdt2.value(), testString);
 		assert.deepEqual(crdt1.getState(), crdt2.getState());
 	});
 
 	it('(One editor) Deletes a word (range)', () => {
 		crdt1.delete(0, 4);
 
-		assert.equal(testString.substring(4, testString.length), crdt1.value());
-		assert.equal(testString.substring(4, testString.length), crdt2.value());
+		assert.equal(crdt1.value(), testString.substring(4, testString.length));
+		assert.equal(crdt2.value(), testString.substring(4, testString.length));
 		assert.deepEqual(crdt1.getState(), crdt2.getState());
 	});
 
 	it('(One editor) Deletes out of range', () => {
 		crdt1.delete(0, testString.length + 1);
 
-		assert.equal('', crdt1.value());
-		assert.equal('', crdt2.value());
+		assert.equal(crdt1.value(), '');
+		assert.equal(crdt2.value(), '');
 		assert.deepEqual(crdt1.getState(), crdt2.getState());
 	});
 
 	it('(One editor) Deletes out of range (negative)', () => {
 		crdt1.delete(0, -1);
 
-		assert.equal(testString, crdt1.value());
-		assert.equal(testString, crdt2.value());
+		assert.equal(crdt1.value(), testString);
+		assert.equal(crdt2.value(), testString);
 		assert.deepEqual(crdt1.getState(), crdt2.getState());
 	});
 
@@ -87,8 +87,8 @@ describe('Delete suite', () => {
 		crdt1.delete(0, 0);
 		crdt2.delete(0, 0);
 
-		assert.equal(testString, crdt1.value());
-		assert.equal(testString, crdt2.value());
+		assert.equal(crdt1.value(), testString);
+		assert.equal(crdt2.value(), testString);
 		assert.deepEqual(crdt1.getState(), crdt2.getState());
 	});
 
@@ -96,8 +96,8 @@ describe('Delete suite', () => {
 		crdt1.delete(testString.length - 1);
 		crdt2.delete(0);
 
-		assert.equal(testString.substring(1, testString.length - 1), crdt1.value());
-		assert.equal(testString.substring(1, testString.length - 1), crdt2.value());
+		assert.equal(crdt1.value(), testString.substring(1, testString.length - 1));
+		assert.equal(crdt2.value(), testString.substring(1, testString.length - 1));
 		assert.deepEqual(crdt1.getState(), crdt2.getState());
 	});
 
@@ -105,8 +105,8 @@ describe('Delete suite', () => {
 		crdt1.delete('This');
 		crdt2.delete('string!');
 
-		assert.equal(testString, crdt1.value());
-		assert.equal(testString, crdt2.value());
+		assert.equal(crdt1.value(), testString);
+		assert.equal(crdt2.value(), testString);
 		assert.deepEqual(crdt1.getState(), crdt2.getState());
 	});
 
@@ -114,8 +114,8 @@ describe('Delete suite', () => {
 		crdt1.delete(2.5);
 		crdt2.delete(0);
 
-		assert.equal(testString.substring(1, testString.length), crdt1.value());
-		assert.equal(testString.substring(1, testString.length), crdt2.value());
+		assert.equal(crdt1.value(), testString.substring(1, testString.length));
+		assert.equal(crdt2.value(), testString.substring(1, testString.length));
 		assert.deepEqual(crdt1.getState(), crdt2.getState());
 	});
 
@@ -123,8 +123,8 @@ describe('Delete suite', () => {
 		crdt1.delete(0, 4);
 		crdt2.delete(0, 2);
 
-		assert.equal(testString.substring(6, testString.length), crdt1.value());
-		assert.equal(testString.substring(6, testString.length), crdt2.value());
+		assert.equal(crdt1.value(), testString.substring(6, testString.length));
+		assert.equal(crdt2.value(), testString.substring(6, testString.length));
 		assert.deepEqual(crdt1.getState(), crdt2.getState());
 	});
 
@@ -132,8 +132,8 @@ describe('Delete suite', () => {
 		crdt1.delete(0, testString.length + 1);
 		crdt2.delete(0, 1);
 
-		assert.equal('', crdt1.value());
-		assert.equal('', crdt2.value());
+		assert.equal(crdt1.value(), '');
+		assert.equal(crdt2.value(), '');
 		assert.deepEqual(crdt1.getState(), crdt2.getState());
 	});
 
@@ -141,8 +141,8 @@ describe('Delete suite', () => {
 		crdt1.delete(0, -1);
 		crdt2.delete(0, 2);
 
-		assert.equal(testString.substring(2, testString.length), crdt1.value());
-		assert.equal(testString.substring(2, testString.length), crdt2.value());
+		assert.equal(crdt1.value(), testString.substring(2, testString.length));
+		assert.equal(crdt2.value(), testString.substring(2, testString.length));
 		assert.deepEqual(crdt1.getState(), crdt2.getState());
 	});
 });
