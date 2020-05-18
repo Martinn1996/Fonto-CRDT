02bf34ea3c53a79eb6f0ca093293ef99255c0073
Martin <m.c.h.li@student.tudelft.nl>
2020-05-18 10:50:53 +0200

    Move all classes to logoot.js

--- src/logoot.js
+++ src/logoot.js
@@ -1,12 +1,8 @@
 const EventEmitter = require('nanobus');
 const inherits = require('inherits');
 
-const Node = require('./class/node');
-const CharacterNode = require('./class/characterNode');
 const Identifier = require('./identifier');
-// const BlockNode = require('./class/blockNode');
 
-const createNodeFromType = require('./util/getNodeType');
 const generateString = require('./util/generateCode');
 
 // eslint-disable-next-line no-use-before-define
@@ -15,6 +11,157 @@ inherits(Logoot, EventEmitter);
 const MIN = 0;
 const MAX = Number.MAX_SAFE_INTEGER;
 const BASE = Math.pow(2, 8);
+
+class Node {
+	constructor(id) {
+		this.id = id;
+		this.children = [];
+		this.parent = null;
+		this.size = 1;
+		this.empty = false;
+		this.type = 'Node';
+	}
+
+	getChildren() {
+		return this.children;
+	}
+
+	_leftmostSearch(child) {
+		let L = 0;
+		let R = this.getChildren().length;
+		let M;
+		while (L < R) {
+			M = Math.floor((L + R) / 2);
+			if (Node.compare(this.children[M].id, child.id) < 0) {
+				L = M + 1;
+			} else {
+				R = M;
+			}
+		}
+		return L;
+	}
+
+	_exactSearch(child) {
+		let L = 0;
+		let R = this.getChildren().length - 1;
+		let M;
+		while (L <= R) {
+			M = Math.floor((L + R) / 2);
+			const comp = Node.compare(this.children[M].id, child.id);
+			if (comp < 0) {
+				L = M + 1;
+			} else if (comp > 0) {
+				R = M - 1;
+			} else {
+				return M;
+			}
+		}
+		return null;
+	}
+
+	adjustSize(amount) {
+		this.size += amount;
+		if (this.parent) this.parent.adjustSize(amount);
+	}
+
+	addChild(child) {
+		child.parent = this;
+		const index = this._leftmostSearch(child);
+		this.children.splice(index, 0, child);
+		this.adjustSize(child.size);
+		return child;
+	}
+
+	removeChild(child) {
+		const index = this._exactSearch(child);
+		if (index === null) return;
+		this.children.splice(index, 1);
+		this.adjustSize(child.size);
+		return child;
+	}
+
+	setEmpty(bool = true) {
+		if (bool === this.empty) return;
+		this.empty = bool;
+		if (bool) {
+			this.adjustSize(-1);
+		} else {
+			this.adjustSize(1);
+		}
+	}
+
+	trimEmpty() {
+		if (!this.parent) return;
+		if (this.empty && this.getChildren().length === 0) {
+			this.parent.removeChild(this);
+			this.parent.trimEmpty();
+		}
+	}
+
+	getPath() {
+		if (!this.parent) return [];
+		return this.parent.getPath().concat([this.id]);
+	}
+
+	getChildById(id) {
+		const index = this._exactSearch({ id });
+		if (index === null) return null;
+		return this.children[index];
+	}
+
+	getChildByPath(path, build, NodeType) {
+		let current = this;
+		let next = null;
+		path.every(id => {
+			next = current.getChildById(id);
+			if (!next && !build) {
+				current = null;
+				return false;
+			}
+			if (!next && build) {
+				next = NodeType ? new NodeType(id) : new Node(id);
+				current.addChild(next);
+				next.setEmpty(true);
+			}
+			current = next;
+			return true;
+		});
+		return current;
+	}
+
+	getOrder() {
+		// -1 to discount the left end node
+		if (!this.parent) return -1;
+		let order = this.parent.getOrder();
+		if (!this.parent.empty) order += 1;
+		for (let i = 0; i < this.parent.getChildren().length; i++) {
+			if (Node.compare(this.parent.getChildren()[i].id, this.id) === 0) break;
+			order += this.parent.getChildren()[i].size;
+		}
+		return order;
+	}
+
+	getChildByOrder(index) {
+		if (index === 0 && !this.empty) return this;
+		let left = this.empty ? 0 : 1;
+		let right = left;
+		for (let i = 0; i < this.getChildren().length; i++) {
+			right += this.getChildren()[i].size;
+			if (left <= index && right > index) {
+				return this.getChildren()[i].getChildByOrder(index - left);
+			}
+			left = right;
+		}
+		return null;
+	}
+
+	walk(fn) {
+		fn(this);
+		this.getChildren().forEach(child => {
+			child.walk(fn);
+		});
+	}
+}
 
 function Logoot(site, state, bias) {
 	EventEmitter.call(this);
@@ -54,6 +201,46 @@ class BlockNode extends Node {
 
 	getChildren() {
 		return this.logoot._root.children;
+	}
+}
+
+class IntermediateNode extends Node {
+	/**
+	 * Constructor for creating intermediate nodes
+	 * @param {*} id for Logoot
+	 */
+	constructor(id) {
+		// Call constructor of parent class
+		super(id);
+
+		this.empty = true;
+		super.type = 'Intermediate';
+	}
+}
+
+class CharacterNode extends Node {
+	/**
+	 * Constructor for creating character nodes
+	 * @param {*} id for Logoot
+	 * @param {*} value to save
+	 */
+	constructor(id, value) {
+		super(id);
+		this.value = value || null;
+		super.type = 'Character';
+	}
+}
+
+function createNodeFromType(nodeType) {
+	switch (nodeType) {
+		case 'Character':
+			return CharacterNode;
+		case 'Block':
+			return BlockNode;
+		case 'Intermediate':
+			return IntermediateNode;
+		default:
+			return Node;
 	}
 }
 
