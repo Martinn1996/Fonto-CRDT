bba0e035d5e05babb06de2df79cae42b7d575ce7
Cas van Rijn <c.vanrijn@student.tudelft.nl>
2020-05-20 14:56:54 +0200

    Added delay test for merge and delete

--- test/delayBlockTest.js
+++ test/delayBlockTest.js
@@ -1,7 +1,7 @@
 const assert = require('chai').assert;
 const Logoot = require('../src/logoot');
 
-describe('Delay tests', () => {
+describe('Delay Block tests', () => {
 	let crdt1, crdt2, ops1, ops2;
 
 	/**
@@ -15,6 +15,14 @@ describe('Delay tests', () => {
 		const block = crdt.insertBlock(index);
 		crdt.insertContentInBlock(value, 0, block.blockId);
 		return block.blockId;
+	}
+
+	function mergeAll() {
+		ops2.forEach(op => crdt2.receive(op));
+		ops1.forEach(op => crdt1.receive(op));
+
+		ops2 = [];
+		ops1 = [];
 	}
 
 	beforeEach(() => {
@@ -32,143 +40,33 @@ describe('Delay tests', () => {
 		});
 	});
 
-    it('should have replica 1 make 2 blocks and merge it while replica2 is adjusting a block', () => {
-        const blockId1 = insertContentInNewBlock(crdt1, 'block 1', 0);
-        const blockId2 = insertContentInNewBlock(crdt1, 'block 2', 0);
-
-        ops2.forEach(op => crdt2.receive(op));
-        crdt2.insertContentInBlock('text voor in block 2');
-        crdt1.
-    })
-
-	it('should converge when 2 sites receive opperations in different order', () => {
-		crdt1.insert('Hallo', 0);
-		crdt2.insert('Meneer', 0);
-
-		for (let i = 0; i < Math.floor(ops2.length / 2); i++) {
-			crdt2.receive(ops2[0]);
-			ops2.shift();
-		}
-
-		for (let i = 0; i < Math.floor(ops1.length / 2); i++) {
-			crdt1.receive(ops1[0]);
-			ops1.shift();
-		}
-
-		ops2.forEach(op => crdt2.receive(op));
-		ops1.forEach(op => crdt1.receive(op));
-
-		assert.equal(crdt1.value(), crdt2.value());
-		assert.deepEqual(crdt1.getState(), crdt2.getState());
-	});
-
-	it('should converge when 2 sites delete and receive opperations in different order', () => {
-		crdt1.setValue('Hallo Meneer');
-		crdt2.setValue('Hallo Meneer');
-
-		crdt2.delete(6, 11);
-		crdt1.delete(0, 3);
-
-		ops2.forEach(op => crdt2.receive(op));
-		ops1.forEach(op => crdt1.receive(op));
-
-		assert.equal(crdt1.value(), crdt2.value());
-		assert.deepEqual(crdt1.getState(), crdt2.getState());
-	});
-
-	it('should converge when 2 sites insert and receive in different order', () => {
-		crdt1.insert('Hal', 0);
-		crdt2.insert('Men', 0);
-		ops2.forEach(op => crdt2.receive(op));
-		ops2 = [];
-		ops1.forEach(op => crdt1.receive(op));
-		ops1 = [];
-		crdt1.insert('lo', 3);
-		crdt2.insert('eer', 4);
+	it('should have replica1 makes 2 blocks and merge it while replica2 is adjusting a block and converge', () => {
+		const blockId1 = insertContentInNewBlock(crdt1, 'block1', 0);
+		const blockId2 = insertContentInNewBlock(crdt1, 'block2', 0);
 
 		ops2.forEach(op => crdt2.receive(op));
 		ops2 = [];
-		ops1.forEach(op => crdt1.receive(op));
-		ops1 = [];
+		crdt2.insertContentInBlock(' text voor in block2', 6, blockId2);
+		crdt1.mergeBlocks(blockId1, blockId2);
 
+		ops2.forEach(op => crdt2.receive(op));
+		ops1.forEach(op => crdt1.receive(op));
+
+		assert.equal(crdt1.value(), 'block1\n\nblock2 text voor in block2');
 		assert.equal(crdt1.value(), crdt2.value());
 		assert.deepEqual(crdt1.getState(), crdt2.getState());
 	});
 
-	it('should converge when 2 sites insert, delete and receive in different order', () => {
-		crdt1.insert('Hallo', 0);
-		crdt2.insert('Goeiendag', 0);
+	it('should converge when replica1 is adding text to a block and replica2 deletes the block', () => {
+		const blockId1 = insertContentInNewBlock(crdt1, 'block1', 0);
+		insertContentInNewBlock(crdt2, 'block2', 0);
 
-		for (let i = 0; i < Math.floor(ops2.length / 2); i++) {
-			crdt2.receive(ops2[0]);
-			ops2.shift();
-		}
+		mergeAll();
 
-		for (let i = 0; i < Math.floor(ops1.length / 2); i++) {
-			crdt1.receive(ops1[0]);
-			ops1.shift();
-		}
+		crdt1.addContentInBlock('dit is blok 1', 0, blockId1);
+		crdt2.deleteBlock(blockId1);
 
-		crdt1.delete(0, 4);
-		crdt2.delete(0, 8);
-
-		for (let i = 0; i < Math.floor(ops2.length / 2); i++) {
-			crdt2.receive(ops2[0]);
-			ops2.shift();
-		}
-
-		for (let i = 0; i < Math.floor(ops1.length / 2); i++) {
-			crdt1.receive(ops1[0]);
-			ops1.shift();
-		}
-
-		crdt1.insert('Hey', 0);
-		crdt2.insert('Goeienavond', 0);
-
-		ops2.forEach(op => crdt2.receive(op));
-		ops1.forEach(op => crdt1.receive(op));
-
-		assert.equal(crdt1.value(), crdt2.value());
-		assert.deepEqual(crdt1.getState(), crdt2.getState());
-	});
-
-	it('should converge when 2 sites insert at different locations and receive in different order', () => {
-		crdt1.insert('Hallo', 0);
-		crdt2.insert('Goeiendag', 0);
-
-		ops2.forEach(op => crdt2.receive(op));
-		ops2 = [];
-		ops1.forEach(op => crdt1.receive(op));
-		ops1 = [];
-
-		crdt1.insert('Meneer', 5);
-		crdt2.insert('Meneer', 9);
-
-		ops2.forEach(op => crdt2.receive(op));
-		ops1.forEach(op => crdt1.receive(op));
-
-		assert.equal(crdt1.value(), crdt2.value());
-		assert.deepEqual(crdt1.getState(), crdt2.getState());
-	});
-
-	it('should converge when 2 sites insert and delete at different locations and receive in different order', () => {
-		crdt1.insert('Hal', 0);
-		crdt2.insert('Goeiendag', 0);
-
-		ops2.forEach(op => crdt2.receive(op));
-		ops2 = [];
-
-		for (let i = 0; i < Math.floor(ops1.length / 2); i++) {
-			crdt1.receive(ops1[0]);
-			ops1.shift();
-		}
-
-		crdt2.delete(0, 5);
-		crdt1.insert('lo', 3);
-
-		ops2.forEach(op => crdt2.receive(op));
-		ops1.forEach(op => crdt1.receive(op));
-
+		assert.equal(crdt1.value(), 'block2\n\n');
 		assert.equal(crdt1.value(), crdt2.value());
 		assert.deepEqual(crdt1.getState(), crdt2.getState());
 	});
