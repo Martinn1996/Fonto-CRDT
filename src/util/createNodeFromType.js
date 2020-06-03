8ece13d88bd02a28660a620d0b2492fcbd3e16af
wanghaojiang1 <W.H.Wang@student.tudelft.nl>
2020-06-03 14:21:52 +0200

    Add split functionality (#53)

--- src/util/createNodeFromType.js
+++ src/util/createNodeFromType.js
@@ -1,6 +1,7 @@
 const BlockNode = require('../class/BlockNode');
 const CharacterNode = require('../class/CharacterNode');
 const Node = require('../class/Node');
+const SplitNode = require('../class/SplitNode');
 
 /**
  * Returns the parsed node type
@@ -13,6 +14,8 @@ function createNodeFromType(nodeType) {
 			return CharacterNode;
 		case 'Block':
 			return BlockNode;
+		case 'Split':
+			return SplitNode;
 		default:
 			return Node;
 	}
