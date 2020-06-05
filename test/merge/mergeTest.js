954eb0f91230e84fb0fb0c6f5c9a0f4f3c19897f
Martin <m.c.h.li@student.tudelft.nl>
2020-06-05 11:47:55 +0200

    More tests for merge

--- test/merge/mergeTest.js
+++ test/merge/mergeTest.js
@@ -293,4 +293,174 @@ describe('merge blocks + delete tests', () => {
 		assert.equal(crdt2.value(), 'text1text2\n\n');
 		assert.deepEqual(crdt1.getState(), crdt2.getState());
 	});
+
+	it('merge 2 blocks and delete on index 0', () => {
+		const block1 = crdt1.insertBlock(0);
+		const block2 = crdt1.insertBlock(1);
+		crdt1.insertContentInBlock('Hoi', 0, block1.blockId);
+		crdt1.insertContentInBlock('Doei', 1, block2.blockId);
+		crdt1.mergeBlocks(block1.blockId, block2.blockId);
+		crdt1.deleteContentInBlock(0, 1, block1.blockId);
+		assert.equal(crdt1.value(), 'oiDoei\n\n');
+		assert.equal(crdt1.value(), crdt2.value());
+		assert.equal(crdt1.getState(), crdt2.getState());
+	});
+
+	it('merge 2 blocks and delete on index 1', () => {
+		const block1 = crdt1.insertBlock(0);
+		const block2 = crdt1.insertBlock(1);
+		crdt1.insertContentInBlock('Hoi', 0, block1.blockId);
+		crdt1.insertContentInBlock('Doei', 1, block2.blockId);
+		crdt1.mergeBlocks(block1.blockId, block2.blockId);
+		crdt1.deleteContentInBlock(1, 1, block1.blockId);
+		assert.equal(crdt1.value(), 'HiDoei\n\n');
+		assert.equal(crdt1.value(), crdt2.value());
+		assert.equal(crdt1.getState(), crdt2.getState());
+	});
+
+	it('merge 2 blocks and delete on index 2', () => {
+		const block1 = crdt1.insertBlock(0);
+		const block2 = crdt1.insertBlock(1);
+		crdt1.insertContentInBlock('Hoi', 0, block1.blockId);
+		crdt1.insertContentInBlock('Doei', 1, block2.blockId);
+		crdt1.mergeBlocks(block1.blockId, block2.blockId);
+		crdt1.deleteContentInBlock(2, 1, block1.blockId);
+		assert.equal(crdt1.value(), 'HoDoei\n\n');
+		assert.equal(crdt1.value(), crdt2.value());
+		assert.equal(crdt1.getState(), crdt2.getState());
+	});
+
+	it('merge 2 blocks and delete on index 3', () => {
+		const block1 = crdt1.insertBlock(0);
+		const block2 = crdt1.insertBlock(1);
+		crdt1.insertContentInBlock('Hoi', 0, block1.blockId);
+		crdt1.insertContentInBlock('Doei', 1, block2.blockId);
+		crdt1.mergeBlocks(block1.blockId, block2.blockId);
+		crdt1.deleteContentInBlock(3, 1, block1.blockId);
+		assert.equal(crdt1.value(), 'Hoioei\n\n');
+		assert.equal(crdt1.value(), crdt2.value());
+		assert.equal(crdt1.getState(), crdt2.getState());
+	});
+
+	it('merge 2 blocks and delete on index 4', () => {
+		const block1 = crdt1.insertBlock(0);
+		const block2 = crdt1.insertBlock(1);
+		crdt1.insertContentInBlock('Hoi', 0, block1.blockId);
+		crdt1.insertContentInBlock('Doei', 1, block2.blockId);
+		crdt1.mergeBlocks(block1.blockId, block2.blockId);
+		crdt1.deleteContentInBlock(4, 1, block1.blockId);
+		assert.equal(crdt1.value(), 'HoiDei\n\n');
+		assert.equal(crdt1.value(), crdt2.value());
+		assert.equal(crdt1.getState(), crdt2.getState());
+	});
+
+	it('merge 2 blocks and delete on index 5', () => {
+		const block1 = crdt1.insertBlock(0);
+		const block2 = crdt1.insertBlock(1);
+		crdt1.insertContentInBlock('Hoi', 0, block1.blockId);
+		crdt1.insertContentInBlock('Doei', 1, block2.blockId);
+		crdt1.mergeBlocks(block1.blockId, block2.blockId);
+		crdt1.deleteContentInBlock(5, 1, block1.blockId);
+		assert.equal(crdt1.value(), 'HoiDoi\n\n');
+		assert.equal(crdt1.value(), crdt2.value());
+		assert.equal(crdt1.getState(), crdt2.getState());
+	});
+
+	it('merge 2 blocks and delete on index 6', () => {
+		const block1 = crdt1.insertBlock(0);
+		const block2 = crdt1.insertBlock(1);
+		crdt1.insertContentInBlock('Hoi', 0, block1.blockId);
+		crdt1.insertContentInBlock('Doei', 1, block2.blockId);
+		crdt1.mergeBlocks(block1.blockId, block2.blockId);
+		crdt1.deleteContentInBlock(6, 1, block1.blockId);
+		assert.equal(crdt1.value(), 'HoiDoe\n\n');
+		assert.equal(crdt1.value(), crdt2.value());
+		assert.equal(crdt1.getState(), crdt2.getState());
+	});
+});
+
+describe('Multiple Merges', () => {
+	let crdt1;
+	let crdt2;
+
+	beforeEach(() => {
+		crdt1 = new Logoot('1');
+		crdt2 = new Logoot('2');
+
+		crdt1.on('operation', op => {
+			crdt2.receive(op);
+		});
+
+		crdt2.on('operation', op => {
+			crdt1.receive(op);
+		});
+	});
+
+	it('should merge at right index after multiple merges', () => {
+		const block1 = crdt1.insertBlock(0);
+		const block2 = crdt1.insertBlock(1);
+		const block3 = crdt1.insertBlock(2);
+
+		crdt1.insertContentInBlock('Hoi', 0, block1.blockId);
+		crdt1.insertContentInBlock('Doei', 0, block2.blockId);
+		crdt1.insertContentInBlock('Terug', 0, block3.blockId);
+		crdt1.mergeBlocks(block1.blockId, block2.blockId);
+		crdt1.mergeBlocks(block1.blockId, block3.blockId);
+		assert.equal(crdt1.value(), 'HoiDoeiTerug\n\n');
+		assert.deepEqual(crdt1.getState(), crdt2.getState());
+	});
+
+	it('should merge at right index after multiple merges with insertions in between', () => {
+		const block1 = crdt1.insertBlock(0);
+		const block2 = crdt1.insertBlock(1);
+
+		crdt1.insertContentInBlock('Hoi', 0, block1.blockId);
+		crdt1.insertContentInBlock('Doei', 0, block2.blockId);
+		crdt1.mergeBlocks(block1.blockId, block2.blockId);
+
+		const block3 = crdt1.insertBlock(2);
+		crdt1.insertContentInBlock('???', 7, block1.blockId);
+		crdt1.insertContentInBlock('Terug', 0, block3.blockId);
+		crdt1.mergeBlocks(block1.blockId, block3.blockId);
+		assert.equal(crdt1.value(), 'HoiDoei???Terug\n\n');
+		assert.deepEqual(crdt1.getState(), crdt2.getState());
+	});
+
+	it('should converge after 8', () => {
+		const block1 = crdt1.insertBlock(0);
+		const block2 = crdt1.insertBlock(1);
+		const block3 = crdt1.insertBlock(2);
+		const block4 = crdt1.insertBlock(3);
+		const block5 = crdt1.insertBlock(4);
+		const block6 = crdt1.insertBlock(5);
+		const block7 = crdt1.insertBlock(6);
+		const block8 = crdt1.insertBlock(7);
+
+		crdt1.insertContentInBlock('1111', 0, block1.blockId);
+		crdt1.insertContentInBlock('2222', 0, block2.blockId);
+		crdt1.insertContentInBlock('3333', 0, block3.blockId);
+		crdt1.insertContentInBlock('4444', 0, block4.blockId);
+		crdt1.insertContentInBlock('5555', 0, block5.blockId);
+		crdt1.insertContentInBlock('6666', 0, block6.blockId);
+		crdt1.insertContentInBlock('7777', 0, block7.blockId);
+		crdt1.insertContentInBlock('8888', 0, block8.blockId);
+
+		crdt1.mergeBlocks(block1.blockId, block2.blockId);
+		console.log(crdt1.value());
+		crdt1.mergeBlocks(block1.blockId, block3.blockId);
+		console.log(crdt1.value());
+		crdt1.mergeBlocks(block1.blockId, block4.blockId);
+		console.log(crdt1.value());
+		crdt1.mergeBlocks(block1.blockId, block5.blockId);
+		console.log(crdt1.value());
+		crdt1.mergeBlocks(block1.blockId, block6.blockId);
+		console.log(crdt1.value());
+		crdt1.mergeBlocks(block1.blockId, block7.blockId);
+		console.log(crdt1.value());
+		crdt1.mergeBlocks(block1.blockId, block8.blockId);
+		console.log(crdt1.value());
+
+		assert.equal(crdt1.value(), '11112222333344445555666677778888\n\n');
+		assert.deepEqual(crdt1.getState(), crdt2.getState());
+	});
 });
