4827ac82250f10cd06737324010703bc70677db1
Martin <m.c.h.li@student.tudelft.nl>
2020-06-08 15:43:50 +0200

    Add more integration tests

--- test/integration/DeleteAndMoveIntegrationTest.js
+++ test/integration/DeleteAndMoveIntegrationTest.js
@@ -1,7 +1,7 @@
 const assert = require('chai').assert;
 const Logoot = require('../../src/logoot');
 
-describe('Offline Support move', () => {
+describe('Offline Support move and delete', () => {
 	let crdt1, crdt2, ops1, ops2;
 
 	beforeEach(() => {
@@ -17,32 +17,7 @@ describe('Offline Support move', () => {
 		});
 	});
 
-	it('should converge after replica 1 merges block 1 to index 2 and replica 2 merges block 1 to index 3', () => {
-		const blockId1 = crdt1.insertBlock(0).blockId;
-		const blockId2 = crdt2.insertBlock(1).blockId;
-		const blockId3 = crdt1.insertBlock(2).blockId;
-
-		crdt1.insertContentInBlock('1', 0, blockId1);
-		crdt2.insertContentInBlock('2', 0, blockId2);
-		crdt1.insertContentInBlock('3', 0, blockId3);
-
-		ops1.forEach(op => crdt1.receive(op));
-		ops2.forEach(op => crdt2.receive(op));
-		ops1 = [];
-		ops2 = [];
-
-		crdt1.moveBlock(blockId1, 2);
-		crdt2.moveBlock(blockId1, 3);
-		ops1.forEach(op => crdt1.receive(op));
-		ops2.forEach(op => crdt2.receive(op));
-		ops1 = [];
-		ops2 = [];
-
-		assert.equal(crdt1.value(), crdt2.value());
-		assert.deepEqual(crdt1.getState().root, crdt2.getState().root);
-	});
-
-	it('should converge after replica 2 replicas move to same index', () => {
+	it('should converge after 1 replica moves a block and the other one deletes the block', () => {
 		const blockId1 = crdt1.insertBlock(0).blockId;
 		const blockId2 = crdt2.insertBlock(1).blockId;
 		const blockId3 = crdt1.insertBlock(2).blockId;
@@ -57,8 +32,7 @@ describe('Offline Support move', () => {
 		ops2 = [];
 
 		crdt1.moveBlock(blockId1, 3);
-
-		crdt2.moveBlock(blockId1, 3);
+		crdt2.deleteBlock(blockId1);
 		ops1.forEach(op => crdt1.receive(op));
 		ops2.forEach(op => crdt2.receive(op));
 		ops1 = [];
