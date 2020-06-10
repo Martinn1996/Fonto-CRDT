acd6e172f88b75648bf54079b1c5ce44960ea7d1
qcllee <33662960+qcllee@users.noreply.github.com>
2020-06-10 14:16:44 +0200

    59 move blocks offline (#60)
    * Add timestamps
    
    * Add wait function
    
    * Add site to comparison
    
    * Add JSDOC
    
    * Remove codecov
    
    * Delete codecov.yml

--- test/move/moveBlockOfflineTest.js
+++ test/move/moveBlockOfflineTest.js
@@ -1,6 +1,12 @@
 const assert = require('chai').assert;
 const Logoot = require('../../src/logoot');
-
+function wait(ms) {
+	const start = new Date().getTime();
+	let end = start;
+	while (end < start + ms) {
+		end = new Date().getTime();
+	}
+}
 describe('Offline Support move', () => {
 	let crdt1, crdt2, ops1, ops2;
 
@@ -17,30 +23,54 @@ describe('Offline Support move', () => {
 		});
 	});
 
-	// it('should converge after replica 1 merges block 1 to index 2 and replica 2 merges block 1 to index 3', () => {
-	// 	const blockId1 = crdt1.insertBlock(0).blockId;
-	// 	const blockId2 = crdt2.insertBlock(1).blockId;
-	// 	const blockId3 = crdt1.insertBlock(2).blockId;
+	it('should converge after replica 1 moves block 1 to index 2 and replica 2 moves block 1 to index 3', () => {
+		const blockId1 = crdt1.insertBlock(0).blockId;
+		const blockId2 = crdt2.insertBlock(1).blockId;
+		const blockId3 = crdt1.insertBlock(2).blockId;
 
-	// 	crdt1.insertContentInBlock('1', 0, blockId1);
-	// 	crdt2.insertContentInBlock('2', 0, blockId2);
-	// 	crdt1.insertContentInBlock('3', 0, blockId3);
+		crdt1.insertContentInBlock('1', 0, blockId1);
+		crdt2.insertContentInBlock('2', 0, blockId2);
+		crdt1.insertContentInBlock('3', 0, blockId3);
 
-	// 	ops1.forEach(op => crdt1.receive(op));
-	// 	ops2.forEach(op => crdt2.receive(op));
-	// 	ops1 = [];
-	// 	ops2 = [];
+		ops1.forEach(op => crdt1.receive(op));
+		ops2.forEach(op => crdt2.receive(op));
+		ops1 = [];
+		ops2 = [];
 
-	// 	crdt1.moveBlock(blockId1, 2);
-	// 	crdt2.moveBlock(blockId1, 3);
-	// 	ops1.forEach(op => crdt1.receive(op));
-	// 	ops2.forEach(op => crdt2.receive(op));
-	// 	ops1 = [];
-	// 	ops2 = [];
+		crdt1.moveBlock(blockId1, 2);
+		crdt2.moveBlock(blockId1, 3);
+		ops1.forEach(op => crdt1.receive(op));
+		ops2.forEach(op => crdt2.receive(op));
+		ops1 = [];
+		ops2 = [];
+		assert.equal(crdt1.value(), crdt2.value());
+		assert.deepEqual(crdt1.getState().root, crdt2.getState().root);
+	});
 
-	// 	assert.equal(crdt1.value(), crdt2.value());
-	// 	assert.deepEqual(crdt1.getState().root, crdt2.getState().root);
-	// });
+	it('should converge after replica 1 moves block 1 to index 2 and replica 2 moves block 1 to index 3 with artificial wait', () => {
+		const blockId1 = crdt1.insertBlock(0).blockId;
+		const blockId2 = crdt2.insertBlock(1).blockId;
+		const blockId3 = crdt1.insertBlock(2).blockId;
+
+		crdt1.insertContentInBlock('1', 0, blockId1);
+		crdt2.insertContentInBlock('2', 0, blockId2);
+		crdt1.insertContentInBlock('3', 0, blockId3);
+
+		ops1.forEach(op => crdt1.receive(op));
+		ops2.forEach(op => crdt2.receive(op));
+		ops1 = [];
+		ops2 = [];
+
+		crdt1.moveBlock(blockId1, 2);
+		wait(10);
+		crdt2.moveBlock(blockId1, 3);
+		ops1.forEach(op => crdt1.receive(op));
+		ops2.forEach(op => crdt2.receive(op));
+		ops1 = [];
+		ops2 = [];
+		assert.equal(crdt1.value(), crdt2.value());
+		assert.deepEqual(crdt1.getState().root, crdt2.getState().root);
+	});
 
 	it('should converge after replica 2 replicas move to same index', () => {
 		const blockId1 = crdt1.insertBlock(0).blockId;
@@ -57,13 +87,11 @@ describe('Offline Support move', () => {
 		ops2 = [];
 
 		crdt1.moveBlock(blockId1, 3);
-
 		crdt2.moveBlock(blockId1, 3);
 		ops1.forEach(op => crdt1.receive(op));
 		ops2.forEach(op => crdt2.receive(op));
 		ops1 = [];
 		ops2 = [];
-
 		assert.equal(crdt1.value(), crdt2.value());
 		assert.deepEqual(crdt1.getState().root, crdt2.getState().root);
 	});
