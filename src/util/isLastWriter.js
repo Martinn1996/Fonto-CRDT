4c5cd5effd1d186d7c11aafe4c315cc3ae147dcc
qcllee <Q.C.L.Lee@student.tudelft.nl>
2020-06-10 14:03:33 +0200

    Add JSDOC

--- src/util/isLastWriter.js
+++ src/util/isLastWriter.js
@@ -1,3 +1,9 @@
+/**
+ * Returns true if the timestamp is the last writer, otherwise return false
+ * @param {JSON} timestamp timestamp of the operation
+ * @param {JSON} otherTimestamp timestamp of the other operation to compare to
+ * @return {boolean} true if the timestamp
+ */
 function isLastWriter(timestamp, otherTimestamp) {
 	if (timestamp.timestamp === otherTimestamp.timestamp) {
 		return timestamp.site < otherTimestamp.site;
